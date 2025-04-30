d3aec0b232887e43d3c87e392ce2590c89acb39a
Marek Sýs <syso@fi.muni.cz>
2025-04-30 07:11:47 +0200

    Edit buggy_python_code_solution.py

--- buggy_python_code_solution.py
+++ buggy_python_code_solution.py
@@ -49,7 +49,7 @@ if __name__ == '__main__':
     print("Vulnerabilities:")
     print("1. Format string vulnerability: use string={person.__init__.__globals__[CONFIG][API_KEY]}")
     print("2. Code injection vulnerability: use string=;print('Own code executed') #")
-    print("3. Yaml deserialization vulnerability: use string=file.yaml")
+    print("3. Yaml deserialization vulnerability: see file_solution.yaml for a solution")
     print("4. Use of assert statements vulnerability: run program with -O argument")
     choice  = input("Select vulnerability: ")
     if choice == "1": 
